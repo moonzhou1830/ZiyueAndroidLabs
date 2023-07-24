9c2292e279deb94d7e0b561d086f23db99e6750b
Ziyue Zhou <zhouziyue2015@gmail.com>
2023-07-24 01:03:39 -0400

    #1 Week 10

--- app/src/main/java/algonquin/cst2335/zhou0224/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/zhou0224/MainActivity.java
@@ -2,11 +2,36 @@ package algonquin.cst2335.zhou0224;
 
 import androidx.appcompat.app.AppCompatActivity;
 //import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import algonquin.cst2335.zhou0224.databinding.ActivityMainBinding;
 
 /**
  * This page is provided to input password,
@@ -22,16 +47,108 @@ public class MainActivity extends AppCompatActivity {
     private EditText et = null;
     /** This holds the button at the bottom of the screen*/
     private Button btn = null;
+    RequestQueue queue = null;
+    RequestQueue queueImage = null;
+    protected String cityName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        queue = Volley.newRequestQueue(this);
+        queueImage = Volley.newRequestQueue(this);
+        ActivityMainBinding binding = ActivityMainBinding.inflate( getLayoutInflater());
+        setContentView(binding.getRoot());
 
         tv = findViewById(R.id.textView);
-        et = findViewById(R.id.editText);
-        btn = findViewById(R.id.button);
+        et = findViewById(R.id.cityTextField);
+        btn = findViewById(R.id.forecastButton);
 
-        btn.setOnClickListener(clk -> {
+        binding.forecastButton.setOnClickListener(clk -> {
+            cityName = binding.cityTextField.getText().toString();
+            String stringURL = "";
+            try {
+                stringURL = "https://api.openweathermap.org/data/2.5/weather?q="
+                        + URLEncoder.encode(cityName,"UTF-8")
+                        + "&appid=7e943c97096a9784391a981c4d878b22&units=metric";
+            } catch (UnsupportedEncodingException e) {
+                throw new RuntimeException(e);
+            }
+            //This goes in the button click handler:
+            JsonObjectRequest request =new JsonObjectRequest(Request.Method.GET,stringURL,null,
+                    (response) -> {
+                try{
+                    JSONArray weatherArray = response.getJSONArray ( "weather" );
+                    JSONObject position0 =  weatherArray.getJSONObject(0);
+                    JSONObject mainObject = response.getJSONObject( "main" );
+
+                    int vis = response.getInt("visibility");
+                    String name = response.getString( "name" );
+
+                    String description = position0.getString("description");
+                    String iconName = position0.getString("icon");
+
+                    double current = mainObject.getDouble("temp");
+                    double min = mainObject.getDouble("temp_min");
+                    double max = mainObject.getDouble("temp_max");
+                    int humidity = mainObject.getInt("humidity");
+
+                    String imageUrl = "https://openweathermap.org/img/w/" + iconName + ".png";
+
+                    runOnUiThread( (  )  -> {
+                                binding.temp.setText("The current temperature is " + current);
+                                binding.temp.setVisibility(View.VISIBLE);
+
+                                binding.minTemp.setText("The min temperature is " + min);
+                                binding.minTemp.setVisibility(View.VISIBLE);
+
+                                binding.maxTemp.setText("The max temperature is " + max);
+                                binding.maxTemp.setVisibility(View.VISIBLE);
+
+                                binding.humidity.setText("The humitidy is " + humidity + "%");
+                                binding.humidity.setVisibility(View.VISIBLE);
+
+                                binding.description.setText("The description is " + description);
+                                binding.description.setVisibility(View.VISIBLE);
+                            });
+                    ImageRequest imgReq = new ImageRequest(imageUrl, new Response.Listener<Bitmap>() {
+                        @Override
+                        public void onResponse(Bitmap bitmap) {
+                            FileOutputStream fOut = null;
+                            try {
+                                fOut = openFileOutput( iconName + ".png", Context.MODE_PRIVATE);
+
+                                bitmap.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+
+                                String filePath = getFilesDir().getAbsolutePath() + "/" + iconName + ".png";
+                                Bitmap savedBitmap = BitmapFactory.decodeFile(filePath);
+                                binding.icon.setImageBitmap( savedBitmap );
+                                binding.icon.setVisibility(View.VISIBLE);
+                                fOut.flush();
+                                fOut.close();
+                            } catch (FileNotFoundException e) {
+                                e.printStackTrace();
+
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+
+                        }
+                    }, 1024, 1024, ImageView.ScaleType.CENTER, null, (error) -> {
+
+                    });
+                    queueImage.add(imgReq);
+                } catch (JSONException e){
+                    throw new RuntimeException(e);
+                }
+                    },
+                    (error) -> {  } );
+            queue.add(request);
+        });
+
+
+
+        /*btn.setOnClickListener(clk -> {
             String password = et.getText().toString();
 
             boolean isPasswordValid = checkPasswordComplexity(password);
@@ -41,7 +158,8 @@ public class MainActivity extends AppCompatActivity {
             } else {
                 textView.setText("You shall not pass!");
             }
-        });
+        });*/
+
     }
 
     /**
